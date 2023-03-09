387d7561974169e162c0b31cb027fefc82d59583
qiaoDong <emailqiao@126.com>
2023-03-09 13:39:53 +0800

    使用SSH将代码上传到Github

---
+++ untitled1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String str = new String("Hello world!");
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+
+        }
+
+    }
+}
